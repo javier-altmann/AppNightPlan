897c9f9bffb5a759ec7a470580aaf48932910548
javier-altmann <javier.altmann@hotmail.com>
2017-11-08 22:43:28 -0300

    Arreglo menu
    Arreglo menu y agrego en layout necesarios. Tambien elimino archivos que no se van a utilizar.

--- Android-App/app/src/main/java/com/example/javieraltmann/nightplan/CrearGrupoActivity.java
+++ Android-App/app/src/main/java/com/example/javieraltmann/nightplan/CrearGrupoActivity.java
@@ -39,17 +39,15 @@ public class CrearGrupoActivity extends AppCompatActivity
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
-        if (id == R.id.nav_camera) {
+        if (id == R.id.nav_inicio) {
             // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
+        } else if (id == R.id.nav_mis_grupos) {
 
-        } else if (id == R.id.nav_slideshow) {
+        } else if (id == R.id.nav_historial) {
 
-        } else if (id == R.id.nav_manage) {
+        } else if (id == R.id.nav_cuenta) {
 
-        } else if (id == R.id.nav_share) {
-
-        } else if (id == R.id.nav_send) {
+        } else if (id == R.id.nav_cerrar_sesion) {
 
         }
 
--- Android-App/app/src/main/java/com/example/javieraltmann/nightplan/GrupoActivity.java
+++ Android-App/app/src/main/java/com/example/javieraltmann/nightplan/GrupoActivity.java
@@ -1,16 +1,57 @@
 package com.example.javieraltmann.nightplan;
 
 import android.os.Bundle;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
 
 /**
  * Created by javier.altmann on 4/11/2017.
  */
 
-public class GrupoActivity extends AppCompatActivity {
+public class GrupoActivity extends AppCompatActivity
+        implements NavigationView.OnNavigationItemSelectedListener {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_grupos);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
+
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(MenuItem item) {
+        // Handle navigation view item clicks here.
+        int id = item.getItemId();
+
+        if (id == R.id.nav_inicio) {
+            // Handle the camera action
+        } else if (id == R.id.nav_mis_grupos) {
+
+        } else if (id == R.id.nav_historial) {
+
+        } else if (id == R.id.nav_cuenta) {
+
+        } else if (id == R.id.nav_cerrar_sesion) {
+
+        }
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
     }
 }
