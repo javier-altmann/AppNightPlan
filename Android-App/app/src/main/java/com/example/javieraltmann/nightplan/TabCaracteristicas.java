871deb91d12f4fb049cc606f755439e2193c0205
javier-altmann <javier.altmann@hotmail.com>
2017-11-12 18:15:35 -0300

    Subo avances del Tab menu de Preferencias
    Subo avances del Tab menu. Falta agregar el menu y arreglar el ancho de
    caracteristicas

---
+++ Android-App/app/src/main/java/com/example/javieraltmann/nightplan/TabCaracteristicas.java
@@ -0,0 +1,22 @@
+package com.example.javieraltmann.nightplan;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by javier.altmann on 12/11/2017.
+ */
+
+public class TabCaracteristicas extends android.support.v4.app.Fragment {
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+
+        View rootView = inflater.inflate(R.layout.fragment_tab_caracteristicas,container,false);
+
+        return rootView;
+    }
+}
---
+++ Android-App/app/src/main/java/com/example/javieraltmann/nightplan/TabGastronomia.java
@@ -0,0 +1,21 @@
+package com.example.javieraltmann.nightplan;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by javier.altmann on 12/11/2017.
+ */
+
+public class TabGastronomia extends android.support.v4.app.Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+
+        View rootView = inflater.inflate(R.layout.fragment_tab_gastronomia,container,false);
+
+        return rootView;
+    }
+}
