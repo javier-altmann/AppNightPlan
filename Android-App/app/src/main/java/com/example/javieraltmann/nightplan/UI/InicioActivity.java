36fcdfc752c751438914acf6d01cd2f31a929725
javier-altmann <javier.altmann@hotmail.com>
2017-11-27 22:51:41 -0300

    #13 Finalizo SplashScreen en pantalla de inicio

--- Android-App/app/src/main/java/com/example/javieraltmann/nightplan/UI/InicioActivity.java
+++ Android-App/app/src/main/java/com/example/javieraltmann/nightplan/UI/InicioActivity.java
@@ -1,15 +1,37 @@
 package com.example.javieraltmann.nightplan.UI;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
+import android.content.pm.ActivityInfo;
 import android.os.Bundle;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.view.WindowManager;
 
 import com.example.javieraltmann.nightplan.R;
 
 public class InicioActivity extends AppCompatActivity {
 
+    private final int DURACION_SPLASH = 500;
+
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_inicio);
+
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        setContentView(R.layout.activity_inicio);
+
+        new Handler().postDelayed(new Runnable(){
+            public void run(){
+                Intent intent = new Intent(InicioActivity.this, LoginActivity.class);
+                startActivity(intent);
+                finish();
+            };
+        }, DURACION_SPLASH);
     }
 }
+
